63af2160c528c4f8b1793cd9fecd7d1fb7be1275
marcelonunes2 <mklelo1990@gmail.com>
2023-06-16 15:02:48 -0300

    pizza

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -16,6 +18,7 @@ pizzaJson.map((item, index) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -76,3 +79,85 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].prices[sizeIndex].toFixed(2)}`;
     });
 });
+
+c('.pizzaInfo--addButton').addEventListener('click', () =>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-Key'))
+    
+
+    let identifier = pizzaJson[modalKey].id+"@"+size
+
+    let key = cart.findIndex((item) =>{
+        return item.identifier === identifier
+    })
+
+    if(key > -1){
+        cart[key].qt += modalQt;
+    }else{
+
+        cart.push({
+        identifier,
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+     })
+    }
+    updateCart()
+    closeModal()
+})
+
+function updateCart(){
+    if(cart.length > 0){
+        c("aside").classList.add("show")
+        c('.cart').innerHTML = ''
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item)=>item.id === cart[i].id )
+            let cartItem = c('.models .cart--item').cloneNode(true)
+
+            let pizaSizeName
+            switch (cart[i].size) {
+                case 0:
+                    pizaSizeName = "P"
+                    break;
+                    
+                    case 1:
+                    pizaSizeName = "M"
+                    
+                    break;
+                    
+                    case 2:
+                    pizaSizeName = "G"
+
+                    break;
+ 
+                default:
+                    break;
+            }
+
+            pizzaName = `${pizzaItem.name}  (${pizaSizeName})` 
+
+            cartItem.querySelector('img').src = pizzaItem.img
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+
+
+                if(cart[i].qt > 1){
+                    cart[i].qt--
+                }else{
+                    cart.splice(i,1)
+                }
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++
+                updateCart()
+            })
+
+
+            c('.cart').append(cartItem)
+        }
+
+    }else{
+        c("aside").classList.remove("show")
+    }
+}
