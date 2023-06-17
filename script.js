3b2ca52c13b46b3163907503bc94bbab7b4415aa
marcelonunes2 <mklelo1990@gmail.com>
2023-06-17 13:41:49 -0300

    NEW

--- script.js
+++ script.js
@@ -109,8 +109,16 @@ function updateCart(){
     if(cart.length > 0){
         c("aside").classList.add("show")
         c('.cart').innerHTML = ''
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
+
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item)=>item.id === cart[i].id )
+            subtotal += pizzaItem.prices[cart[i].size] * cart[i].qt;
+
+
             let cartItem = c('.models .cart--item').cloneNode(true)
 
             let pizaSizeName
@@ -157,6 +165,12 @@ function updateCart(){
             c('.cart').append(cartItem)
         }
 
+        desconto = subtotal * 0.1
+        total = subtotal - desconto
+
+        c(".subtotal span:last-child").innerHTML =  `R$ ${subtotal.toFixed(2)}`
+        c(".desconto span:last-child").innerHTML =  `R$ ${desconto.toFixed(2)}`
+        c(".total span:last-child").innerHTML =  `R$ ${total.toFixed(2)}`
     }else{
         c("aside").classList.remove("show")
     }
